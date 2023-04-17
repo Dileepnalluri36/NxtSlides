3572a1a6ed98f89faa40488e053771539dc03fbe
Dileepnalluri36 <dp7730899@gmail.com>
2023-04-17 09:53:18 +0000

    Final Code

--- src/components/EachSlide/index.js
+++ src/components/EachSlide/index.js
@@ -1,17 +1,24 @@
 import './index.css'
 
 const EachSlide = props => {
-  const {eachSlide, activeSlideChange} = props
+  const {eachSlide, isActive, slideIndex, activeSlideChange} = props
   const {id, heading, description} = eachSlide
 
+  const activeClassName = isActive ? 'active-bg' : ''
   const changeActiveSlide = () => {
     activeSlideChange(id)
   }
 
   return (
-    <li onClick={changeActiveSlide} className="each_slide_item">
-      <h1 className="item_heading">{heading}</h1>
-      <p>{description}</p>
+    <li
+      onClick={changeActiveSlide}
+      className={`each_slide_item ${activeClassName}`}
+    >
+      <p className="indexNum">{slideIndex}</p>
+      <div className="slide_text_container">
+        <h1 className="item_heading">{heading}</h1>
+        <p className="desc">{description}</p>
+      </div>
     </li>
   )
 }
--- src/components/NxtSlides/index.js
+++ src/components/NxtSlides/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable prefer-destructuring */
 import {v4 as uuidv4} from 'uuid'
 import './index.css'
 import {Component} from 'react'
+import {MdOutlineDeleteOutline} from 'react-icons/md'
 import EachSlide from '../EachSlide'
 
 const initialSlidesList = [
@@ -104,6 +106,22 @@ class NxtSlides extends Component {
     }))
   }
 
+  changeByKeyDown = event => {
+    if (event.key === 'Enter') {
+      this.setState(prevState => ({
+        isEditingHeading: !prevState.isEditingHeading,
+      }))
+    }
+  }
+
+  changeByKeyDownDesc = event => {
+    if (event.key === 'Enter') {
+      this.setState(prevState => ({
+        isEditingDesc: !prevState.isEditingDesc,
+      }))
+    }
+  }
+
   addNewSlide = () => {
     const {slidesList, activeSlide} = this.state
 
@@ -114,13 +132,35 @@ class NxtSlides extends Component {
     }
     const activeSlideIndex = slidesList.indexOf(activeSlide)
 
-    const updatedSlidesList = slidesList.splice(
-      activeSlideIndex + 1,
-      0,
-      newSlide,
-    )
+    slidesList.splice(activeSlideIndex + 1, 0, newSlide)
 
     this.setState({slidesList, activeSlide: newSlide})
+  }
+
+  deleteActiveSlide = () => {
+    const {slidesList, activeSlide} = this.state
+
+    const activeSlideIndex = slidesList.indexOf(activeSlide)
+
+    const updatedSlides = slidesList.filter(
+      eachSlide => eachSlide.id !== activeSlide.id,
+    )
+
+    console.log(activeSlideIndex, slidesList.length)
+    let updatedActiveSlide = activeSlide
+    if (activeSlideIndex === 0) {
+      updatedActiveSlide = slidesList[1]
+    } else if (activeSlideIndex === slidesList.length - 1) {
+      updatedActiveSlide = slidesList[slidesList.length - 2]
+    } else {
+      updatedActiveSlide = slidesList[activeSlideIndex - 1]
+    }
+
+    this.setState({slidesList: updatedSlides, activeSlide: updatedActiveSlide})
+  }
+
+  resetToPara = () => {
+    this.setState({isEditingDesc: false, isEditingHeading: false})
   }
 
   render() {
@@ -131,55 +171,80 @@ class NxtSlides extends Component {
       isEditingDesc,
     } = this.state
     const {heading, description} = activeSlide
+
     return (
       <div className="main_container">
         <div className="header_container">
           <img
+            className="logo-img"
             src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
             alt="nxt slides logo"
           />
-          <h1 className="nxtWave-heading">Nxt Slides</h1>
+          <h1 className="nxtSlides-heading">Nxt Slides</h1>
         </div>
         <button onClick={this.addNewSlide} className="newButton" type="button">
           <img
+            className="plusImg"
             src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-plus-icon.png"
             alt="new plus icon"
           />
-          <p>New</p>
+          <p className="newText">New</p>
         </button>
         <div className="slides_container">
           <ul className="slides_list">
             {slidesList.map(eachSlide => (
               <EachSlide
+                slideIndex={slidesList.indexOf(eachSlide) + 1}
                 activeSlideChange={this.activeSlideChange}
                 eachSlide={eachSlide}
+                isActive={eachSlide.id === activeSlide.id}
                 key={eachSlide.id}
               />
             ))}
           </ul>
 
           <div className="active_slide_container">
-            {isEditingHeading ? (
-              <input
-                className="active_slide_heading"
-                value={heading}
-                onChange={this.changeActiveSlideHeading}
-                onBlur={this.changeTag}
+            <div className="button_div">
+              <MdOutlineDeleteOutline
+                className="delete_icon"
+                onClick={this.deleteActiveSlide}
               />
-            ) : (
-              <p onClick={this.changeTag}>{heading}</p>
-            )}
+            </div>
 
-            {isEditingDesc ? (
-              <input
-                className="active_slide_desc"
-                value={description}
-                onChange={this.changeActiveSlideDesc}
-                onBlur={this.changeTagDesc}
-              />
-            ) : (
-              <p onClick={this.changeTagDesc}>{description}</p>
-            )}
+            <div className="active_slide_text_container">
+              {isEditingHeading ? (
+                <input
+                  className="active_slide_heading"
+                  value={heading}
+                  onChange={this.changeActiveSlideHeading}
+                  onBlur={this.changeTag}
+                  onKeyDown={this.changeByKeyDown}
+                />
+              ) : (
+                <p
+                  className="active_slide_heading_text"
+                  onClick={this.changeTag}
+                >
+                  {heading}
+                </p>
+              )}
+              {isEditingDesc ? (
+                <input
+                  className="active_slide_desc"
+                  value={description}
+                  onChange={this.changeActiveSlideDesc}
+                  onBlur={this.changeTagDesc}
+                  onKeyDown={this.changeByKeyDownDesc}
+                />
+              ) : (
+                <p
+                  className="active_slide_desc_text"
+                  onClick={this.changeTagDesc}
+                >
+                  {description}
+                </p>
+              )}
+            </div>
           </div>
         </div>
       </div>
